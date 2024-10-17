be11f0d451f6746998a97da6ee4d5d842675042c
ZawLinNaing24 <zawlinnaing52@gmail.com>
2024-10-18 04:48:23 +0630

    initail commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,44 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  ValueNotifier<int> count = ValueNotifier(0);
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    debugPrint("Build the widget");
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Value Notifier"),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ValueListenableBuilder(
+                    valueListenable: count,
+                    builder: (context, value, child) =>
+                        Text("Your Counter value is : ${count.value}")),
+                ElevatedButton(
+                    onPressed: () {
+                      count.value++;
+
+                      debugPrint(count.value.toString());
+                    },
+                    child: const Icon(Icons.add))
+              ],
+            ),
+          ),
         ),
       ),
     );
